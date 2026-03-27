089ea3c8ac80f016d33f37098599e2799dfc3889
Getman Dmitry <dima.getman12112001@gmail.com>
2026-03-27 17:10:46 +0300

    Добавил контроллер в web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Http\Controllers\TelegramBotWebhookController;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/telegram-bot/webhook/'.config('telegram_bot.webhook_route_key'), TelegramBotWebhookController::class)
+    ->withoutMiddleware('web')
+    ->middleware(SubstituteBindings::class)
+    ->name('telegram-bot-webhook');
